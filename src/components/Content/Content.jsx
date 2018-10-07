9da8d06795ca0071e58a7da1d78adefb9e0b749d
Pete Jeffryes <pjeffryes@tendrilinc.com>
2018-10-07 17:37:10 -0600

    added subtitle

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -52,9 +52,11 @@ class Content extends React.Component {
   renderModal() {
     const {card, frequency} = this.state;
 
-    const body = `The ${card.title} Plan for $${card.pricePer[frequency]} /${frequency}. Fusce suscipit libero eget elit. Praesent dapibus. Nullam rhoncus aliquam metus. Nulla non arcu lacinia neque faucibus fringilla. Nullam eget nisl. Etiam dictum tincidunt diam. Curabitur bibendum justo non orci. Duis condimentum augue id magna semper rutrum. Curabitur sagittis hendrerit ante. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Nullam at arcu a est sollicitudin euismod. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis.`;
+    const subtitle = `The ${card.title} Plan for $${card.pricePer[frequency]} /${frequency}.`;
+    const body = `Fusce suscipit libero eget elit. Praesent dapibus. Nullam rhoncus aliquam metus. Nulla non arcu lacinia neque faucibus fringilla. Nullam eget nisl. Etiam dictum tincidunt diam. Curabitur bibendum justo non orci. Duis condimentum augue id magna semper rutrum. Curabitur sagittis hendrerit ante. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Nullam at arcu a est sollicitudin euismod. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis.`;
     return <Modal
       title={'Sign Up!'}
+      subtitle={subtitle}
       body={body}
       buttonText={'Confirm'}
       closeModal={() => this.toggleModal()}/>;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,7 @@ import './styles.scss';
 class Modal extends React.Component {
   render() {
 
-    const {title, body, buttonText} = this.props;
+    const {title, subtitle, body, buttonText} = this.props;
 
     return (
       <div className='backdrop'>
@@ -13,6 +13,11 @@ class Modal extends React.Component {
           <div className='modal__header'>
             <p>{title}</p>
             <p onClick={this.props.closeModal} className='close'></p>
+          </div>
+          <div className='modal__subtitle'>
+            <p>
+              {subtitle}
+            </p>
           </div>
           <div className='modal__body'>
             <p>
@@ -30,6 +35,8 @@ class Modal extends React.Component {
 
 Modal.propTypes = {
   title: PropTypes.string,
+  subtitle: PropTypes.string,
+  body: PropTypes.string,
   closeModal: PropTypes.func,
 };
 
